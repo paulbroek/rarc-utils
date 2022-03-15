ccec0cd0406cf149643bb8c452332941e7ca249a
paulbroek <pauliusbroek@hotmail.com>
2022-03-15 02:53:35 +0100

    rename sqlalchemy.py to sqlalchemy_base.py to prevent sqlalchemy library name collisions

--- rarc_utils/replay_log.py
+++ rarc_utils/replay_log.py
@@ -18,11 +18,12 @@ import configparser
 from pathlib import Path
 
 import redis
+
 from rarc.config.redis import redis as rk
 import rarc.config.redis
 from rarc_utils.log import setup_logger, read_json_log_file, read_json_log_redis
 from rarc_utils.misc import AttrDict
-from rarc_utils.sqlalchemy import get_session
+from rarc_utils.sqlalchemy_base import get_session
 
 log_fmt = "%(asctime)s - %(module)-16s - %(lineno)-4s - %(funcName)-16s - %(levelname)-7s - %(message)s"  # name
 logger = setup_logger(cmdLevel=logging.INFO, saveFile=0, savePandas=1, jsonLogger=0, color=1, fmt=log_fmt) # URGENT WARNING
