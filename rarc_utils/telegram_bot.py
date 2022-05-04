4b5282590359b26ad4824a260763f28db7e0fea0
paulbroek <pauliusbroek@hotmail.com>
2022-05-04 16:33:54 +0200

    v0.0.10, include telegram_bot utility functions

---
+++ rarc_utils/telegram_bot.py
@@ -0,0 +1,20 @@
+""" telegram_bot.py 
+
+    utility methods for telegram bots
+"""
+
+def toEscapeMsg(msg: str) -> str:
+    """markdown messages cannot contain markers of style like _ * = etc."""
+
+    return (
+        msg.replace("[", "\\[")
+        .replace("(", "\\(")
+        .replace(")", "\\)")
+        .replace("-", "\\-")
+        .replace("+", "\\+")
+        .replace("=", "\\=")
+        .replace(".", "\\.")
+        .replace("`", "\\`")
+    )
+    # replace("_", "\\_")
+    # .replace("*", "\\*") \
