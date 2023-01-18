c00495986c670b56567a0f774db004bd657bd203
Paul Broek <pauliusbroek@hotmail.com>
2023-01-18 09:47:08 +0100

    hide passwd in repr

--- rarc_utils/models/psql_config.py
+++ rarc_utils/models/psql_config.py
@@ -1,20 +1,31 @@
 """psql_config.py.
 
-postgresql configuration class to enforce strong typing on configurations
+postgresql configuration class to enforce strong typing on configurations 
 passed from .db.{test,prod}.env files
 """
 
-from attrs import field, fields_dict, frozen, validators
+from attrs import asdict, field, fields_dict, frozen, validators
 
 
 @frozen
 class psqlConfig:
     """Psql configuration."""
+
+    HIDDEN_FIELD = "PG_PASSWD"
     PG_HOST: str = field(validator=[validators.instance_of(str)])
     PG_PORT: int = field(converter=int)
     PG_USER: str = field(validator=[validators.instance_of(str)])
     PG_PASSWD: str = field(validator=[validators.instance_of(str)])
     PG_DB: str = field(validator=[validators.instance_of(str)])
+
+    def __repr__(self):
+        """Do not display `HIDDEN_FIELD` class attribute."""
+        fields = asdict(self)
+        fields = {
+            k: "..." if k == psqlConfig.HIDDEN_FIELD else v for k, v in fields.items()
+        }
+        print("I ran!")
+        return str(fields)
 
 
 psqlKeys = fields_dict(psqlConfig)
