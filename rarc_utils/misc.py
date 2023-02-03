61eaa65c7dd43fe2ca8b6628f8ca871fcd596232
Paul Broek <pauliusbroek@hotmail.com>
2023-02-03 14:44:57 +0100

    add validate_url method

--- rarc_utils/misc.py
+++ rarc_utils/misc.py
@@ -8,6 +8,7 @@ import itertools
 import logging
 import os
 import random
+import re
 import signal
 import subprocess
 import sys
@@ -174,6 +175,19 @@ def run_in_executor(f):
         return loop.run_in_executor(None, partial(f, *args, **kwargs))
 
     return inner
+
+
+def validate_url(url: str) -> bool:
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"  # http:// or https://
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+        r"localhost|"  # localhost...
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+    return re.match(regex, url) is not None
 
 
 class AttrDict(dict):
